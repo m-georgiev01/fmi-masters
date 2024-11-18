297ac09689e11f8dac83951333d7fbb52502e7a3
Martin Georgiev <83757143+m-georgiev01@users.noreply.github.com>
2024-11-18 22:42:34 +0200

    Course project 1 - Initial commit

---
+++ rent-a-car-api/src/test/java/org/rentacarapi/RentACarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rentacarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
